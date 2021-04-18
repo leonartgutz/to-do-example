acf37744657c242d17de7a252a23c0e8983fd659
Leonart Gutz <leonartgutz@hotmail.com>
2021-04-18 12:46:09 -0700

    First Commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    quotes: ['error', 'single', {
+      allowTemplateLiterals: true,
+    }],
+    semi: ['error', 'always'],
+  },
+};
